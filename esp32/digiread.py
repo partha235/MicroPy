547a0055afd6d534263e4daa8ffb1563449d9151
Parthasarathy <parthaa235@gmail.com>
2022-08-01 13:52:26 +0530

    working

--- esp32/digiread.py
+++ esp32/digiread.py
@@ -3,9 +3,8 @@ from utime import sleep_ms
 
 sen=Pin(4,Pin.IN)
 def sen_read():
-    x=sen.value()
+    x=sen.value() # to read a value of Pin.
     return x
 while True:
-   # x=sen.value()  # to read a value of Pin.
     print(sen_read())
     sleep_ms(100)
