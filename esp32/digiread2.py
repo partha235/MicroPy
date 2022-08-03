45e0f60881aa7df78bdf6db2895629d3e69a0897
Parthasarathy <parthaa235@gmail.com>
2022-08-03 17:10:29 +0530

    working

--- esp32/digiread2.py
+++ esp32/digiread2.py
@@ -3,8 +3,11 @@ from utime import sleep_ms
 
 sen=Pin(4,Pin.IN)
 def sen_read():
+    global x      # declaring the variable as global.
     x=sen.value() # to read a value of Pin.
-    print(x)
+    return x      # this program is to find difference between "return" & "print".
+
 while True:
     sen_read()
+    print(x)
     sleep_ms(100)
---
+++ esp32/digiread3.py
@@ -0,0 +1,18 @@
+"""this program is to demonstrate the pin with internal "Pullup" & "pulldown" resistor value. it increase
+the accuracy of sensor read value."""
+from machine import Pin
+from utime import sleep_ms  
+
+sen=Pin(4,Pin.IN,Pin.PULL_UP)     # use when sensor output is "LOW" by default.
+sen2=Pin(2,Pin.IN,Pin.PULL_DOWN)  # use when sensor output is "HIGH" by default.
+def sen_read():
+    global x,y      # declaring the variables as global.
+    x=sen.value()   # to read a value of Pin.
+    y=sen2.value()
+    return x,y        
+
+while True:
+    sen_read()
+    print(x,"x vale")
+    print(y,"y value")
+    sleep_ms(100)
