c4ceda2c2e46f09a9dad006286b5d15b8f0755f3
partha235 <parthaa235@gmail.com>
2024-01-01 18:33:43 +0530

    working

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 """ as file named main run as default in micropython you can reprogram it"""
-from machine import Pin
-from time import sleep_ms
-sen=Pin(7,Pin.IN)
+from machine import Pin,PWM
+import time
+
+serv = PWM(Pin(12, mode=Pin.OUT),50)
+
+# 0.5ms/20ms = 0.025 = 2.5% duty cycle
+# 2.4ms/20ms = 0.12 = 12% duty cycle
+
+# 0.025*1024=25.6
+# 0.12*1024=122.88
+
 while True:
-    print(sen.value(),"saga")
-    sleep_ms(100)
+    serv.duty(26)
+    time.sleep(1)
+    print("1st loop")
+    serv.duty(123)
+    time.sleep(1)
