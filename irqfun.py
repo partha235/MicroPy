d12b821d1e60e9126abb2ded8882f2af4d15bd0d
partha235 <parthaa235@gmail.com>
2024-07-04 06:55:31 +0530

    interrupter

--- irqfun.py
+++ irqfun.py
@@ -1,4 +1,10 @@
-from machine import Pin
+# This program is for interrupt function that attached to the pin.
+# Based on sensor output the trigger mode of irq is alloted.
+# For sensor based on active low state "Pin.IRQ_FALLING" is used.
+# For sensor based on active high state "Pin.IRQ_RISING" is used
+
+
+from machine import Pin,enable_irq,disable_irq
 from utime  import sleep_ms
 sen=Pin(2,Pin.IN,Pin.PULL_DOWN)
 
@@ -9,6 +15,6 @@ def sen_re(pin):
     led.off()
 
 while True:
-    sen.irq(trigger=Pin.IRQ_RISING,handler=sen_re)
+    sen.irq(trigger=Pin.IRQ_RISING,handler=sen_re) 
     print(sen.value())
     sleep_ms(50)
