10f97ffdaf97daf9d19d6cf021f70387069f6a8e
partha235 <parthaa235@gmail.com>
2024-10-17 20:31:39 +0530

    blink pin change

--- blink.py
+++ blink.py
@@ -1,7 +1,7 @@
 from machine import Pin
 from time import sleep_ms
 
-led=Pin(13,Pin.OUT) #in esp32 dev kit Pin 2 has onboard led.
+led=Pin(2,Pin.OUT) #in esp32 dev kit Pin 2 has onboard led.
 
 while True:
     led.on()
